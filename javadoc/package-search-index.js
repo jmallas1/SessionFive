c3d8380685eb5198f54d68d08b7a5e5dafff85da
Jared R. Mallas <jrtytn@gmail.com>
2018-09-24 17:17:31 -0500

    date math and javadoc

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"org.jrm"},{"l":"org.jrm.io"},{"l":"us.jmallas1.enums"}]
+packageSearchIndex = [{"l":"org.jrm"},{"l":"org.jrm.io"},{"l":"us.jmallas1.bignums"},{"l":"us.jmallas1.dates"},{"l":"us.jmallas1.enums"},{"l":"us.jmallas1.numwrappers"}]
